83e87ad199cfb80d75473cc6eda5a8ed68511b3e
Bernardo Tolosa <bernardotolosajr@gmail.com>
2018-04-20 13:50:24 +0800

    added component test for plain impl

---
+++ test/plain/components/addtodo.test.js
@@ -0,0 +1,39 @@
+import { mount } from '@vue/test-utils';
+import expect from 'expect';
+import AddTodo from '../../../plain/components/addtodo.vue';
+
+describe('AddTodo', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(AddTodo);
+  });
+
+  it('emit add new todo when value is present', () => {
+    const newTodo = wrapper.find('#new-todo');
+
+    newTodo.element.value = 'new todo';
+    newTodo.trigger('input');
+
+    newTodo.trigger('keyup.enter');
+
+    const {
+      id,
+      content,
+      completed, editing
+    } = wrapper.emitted('create-todo')[0][0];
+
+    expect(id).toBeTruthy();
+    expect(content).toBe('new todo');
+    expect(completed).toBeFalsy();
+    expect(editing).toBeFalsy();
+  });
+
+  it('not emit when value is empty', () => {
+    const newTodo = wrapper.find('#new-todo');
+
+    newTodo.trigger('keyup.enter');
+
+    expect(wrapper.emitted('create-todo')).toBeFalsy();
+  });
+});

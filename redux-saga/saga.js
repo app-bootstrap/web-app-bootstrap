5f508f6a945fd554450989f9df379c398d51c51b
xudafeng <xudafeng@126.com>
2019-12-01 15:56:57 +0800

    chore: tweak babel

--- redux-saga/saga.js
+++ redux-saga/saga.js
@@ -1,13 +1,14 @@
-import { delay } from 'redux-saga';
 import { put, takeEvery } from 'redux-saga/effects';
 
-async function addAsync() {
-  await delay(2000);
-  await put({ type: 'ADD' });
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+function * addAsync() {
+  yield delay(2000);
+  yield put({ type: 'ADD' });
 }
 
-async function mySaga() {
-  await takeEvery('ADDASYNC', addAsync);
+function * mySaga() {
+  yield takeEvery('ADDASYNC', addAsync);
 }
 
 export default mySaga;

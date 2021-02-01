26b20ec05050d90769b53b1d839160825e9302a2
xudafeng <xudafeng@126.com>
2021-02-01 13:59:14 +0800

    feat: tweak router

--- react-router/Home.jsx
+++ react-router/Home.jsx
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Home = (props) => {
   console.log('run home', props);
+  const goFirst = () => {
+    props.history.push('/first');
+  };
   return (
     <div>
       <h1>home</h1>
       <div>
-        <div onClick={() => { props.history.push('/first'); }}>go to first</div>
+        <div onClick={goFirst}>go to first</div>
       </div>
       <div>
         <Link to="/second">go to second</Link>
--- react-router/index.js
+++ react-router/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { createBrowserHistory, createHashHistory } from 'history';
 
 import Home from './Home.jsx';
@@ -15,11 +15,15 @@ const browserHistory = createBrowserHistory({
 });
 
 const history = hashHistory || browserHistory;
+const loggedIn = true;
 
 const app = (
   <Router history={history}>
     <Switch>
-      <Route exact path={'/'} component={Home} />
+      <Route exact path="/">
+        {loggedIn ? <Redirect to="/home" /> : <div />}
+      </Route>
+      <Route exact path={'/home'} component={Home} />
       <Route exact path={'/first'} component={First} />
       <Route exact path={'/second'} component={Second} />
     </Switch>

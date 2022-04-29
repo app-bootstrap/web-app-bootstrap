2a36ee032b3580d6977ab5f0410cafe2530ac114
达峰的夏天 <xudafeng@126.com>
2022-04-30 01:10:10 +0800

    test: add videos (#33)

--- test/app-bootstrap.test.js
+++ test/app-bootstrap.test.js
@@ -3,6 +3,7 @@
 import {
   webpackHelper
 } from 'macaca-wd';
+import path from 'path';
 
 const {
   driver,
@@ -18,7 +19,10 @@ describe('test/app-bootstrap.test.js', () => {
           browserName: 'chromium',
           width: 375,
           height: 667,
-          deviceScaleFactor: 2
+          deviceScaleFactor: 2,
+          recordVideo: {
+            dir: path.resolve(__dirname, '..', 'reports', 'screenshots'),
+          },
         });
     });
 
@@ -30,6 +34,7 @@ describe('test/app-bootstrap.test.js', () => {
     afterEach(function () {
       return driver
         .coverage()
+        .saveVideos(this)
         .saveScreenshots(this);
     });
 
